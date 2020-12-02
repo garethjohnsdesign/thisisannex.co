9dfd090b03c8450bc078bb6600df393b462aa323
Gareth Johns <garethjohns@gareths-imac.lan>
2020-12-02 21:43:51 +0000

    Initial Commit

--- web/assets/js/app.js
+++ web/assets/js/app.js
@@ -101,15 +101,29 @@ $(function () {
 // 5. Carousel
 // -----------
 
+
+// https://stackoverflow.com/a/36389263
+var getTimeout = function(){var e=setTimeout,b={};setTimeout=function(a,c){var d=e(a,c);b[d]=[Date.now(),c];return d};return function(a){return(a=b[a])?Math.max(a[1]-Date.now()+a[0],0):NaN}}();
+
+// https://curtistimson.co.uk/post/js/default-negative-variables-to-zero-in-javascript/
+function sanitisePercentage(i){
+  return Math.min(100,Math.max(0,i));
+}
+
+// Slider
+var percentTime;
+var tick;
+var progressBar = document.querySelector('.swiper-hero-progress');
+
 // configure Swiper to use modules
 Swiper.use([Navigation, Pagination, EffectFade, Keyboard, Autoplay]);
 
 const swiper = new Swiper();
-
 var mySwiper = new Swiper('.swiper-container', {
   // Optional parameters
  autoplay: {
     delay: 5000,
+    disableOnInteraction: false
   },
  effect: 'fade',
  fadeEffect: {
@@ -133,7 +147,55 @@ var mySwiper = new Swiper('.swiper-container', {
   keyboard: {
     enabled: true,
   },
+  on: {
+    slideChange: function() {
+          var swiper = this;
+      var defaultSlideDelay = swiper.params.autoplay.delay;
+      var currentIndex = swiper.realIndex + 1;
+      var currentSlide = swiper.slides[currentIndex];
+      var currentSlideDelay = currentSlide.getAttribute('data-swiper-autoplay') || defaultSlideDelay;
+      
+      updateSwiperProgressBar(progressBar, currentSlideDelay);
+    }
+  }
 })
+
+function updateSwiperProgressBar(bar, slideDelay) {
+
+  function startProgressBar() {
+    resetProgressBar();
+    tick = setInterval(progress, 50);
+  }
+
+  function progress() {
+
+    var timeLeft = getTimeout(mySwiper.autoplay.timeout);
+
+    if ( mySwiper.autoplay.running && !mySwiper.autoplay.paused ) {
+      percentTime = sanitisePercentage(100 - Math.round(timeLeft / slideDelay * 100));
+      bar.style.width = percentTime + '%';
+
+      if (percentTime > 100) {
+        resetProgressBar();
+      }
+    }
+
+    if ( mySwiper.autoplay.paused ) {
+      percentTime = 0;
+      bar.style.width = 0;
+    }
+
+  }
+
+  function resetProgressBar() {
+    percentTime = 0;
+    bar.style.width = 0;
+    clearInterval(tick);
+  }
+
+  startProgressBar();
+
+}
 
 
 /*
